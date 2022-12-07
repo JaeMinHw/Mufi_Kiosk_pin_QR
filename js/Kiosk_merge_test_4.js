616fe307ddbd66dd0c45ad3545a25c4ace8466dd
JM <bosik99@naver.com>
2022-12-08 02:02:16 +0900

    file_sum_sol

--- js/Kiosk_merge_test_4.js
+++ js/Kiosk_merge_test_4.js
@@ -105,25 +105,10 @@ function saveImage() {
       // 인증실패시 에러메시지 출력
     }
   })
-  chmod();
+
 }
 
 
-function chmod() {
-  $.ajax({
-    type: 'GET',
-    url: 'http://127.0.0.1:5000/chmod/' + sessionStorage.getItem('pin'),
-    dataType: 'text',
-    success: function (data) {
-      console.log(data);
-    },
-    error: function (result) {
-      // error -> 인증실패 Alert 출력
-      alert('오류입니다. 다시 시도 후 문제가 또 발생하면 안내자에게 문의해주세요.')
-      // 인증실패시 에러메시지 출력
-    }
-  })
-}
 
 
 function day_re() {
